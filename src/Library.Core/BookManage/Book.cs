949dd6a4be345537076adb9ce23861d26c3a6d76
Liu233w <wwwlsmcom@outlook.com>
2017-09-13 16:19:27 +0800

    更新book数据表

--- src/Library.Core/BookManage/Book.cs
+++ src/Library.Core/BookManage/Book.cs
@@ -1,10 +1,15 @@
-﻿using Abp.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
 
 namespace Library.BookManage
 {
+    [Table("AppBooks")]
     public class Book : Entity<long>
     {
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Isbn { get; set; }
+        public string Author { get; set; }
+        public string Publish { get; set; }
     }
 }
