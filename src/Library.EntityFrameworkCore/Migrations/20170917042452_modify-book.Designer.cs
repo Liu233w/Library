1f3bc23f8463c901c03bbe9769637c3edb70632f
Liu233w <wwwlsmcom@outlook.com>
2017-09-17 12:43:03 +0800

    更新数据库
    for 949dd6a4be345537076adb9ce23861d26c3a6d76

--- src/Library.EntityFrameworkCore/Migrations/20170917042452_modify-book.Designer.cs
+++ src/Library.EntityFrameworkCore/Migrations/20170917042452_modify-book.Designer.cs
@@ -15,9 +15,10 @@ using System;
 namespace Library.Migrations
 {
     [DbContext(typeof(LibraryDbContext))]
-    partial class LibraryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170917042452_modify-book")]
+    partial class modifybook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Library.EntityFrameworkCore/Migrations/20170917042452_modify-book.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Migrations
+{
+    public partial class modifybook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "AppBooks");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "AppBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Isbn",
+                table: "AppBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publish",
+                table: "AppBooks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppBooks",
+                table: "AppBooks",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppBooks",
+                table: "AppBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "AppBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Isbn",
+                table: "AppBooks");
+
+            migrationBuilder.DropColumn(
+                name: "Publish",
+                table: "AppBooks");
+
+            migrationBuilder.RenameTable(
+                name: "AppBooks",
+                newName: "Books");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+    }
+}
