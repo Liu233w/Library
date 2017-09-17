b96d9027fcd2be225a35c1e639f31e051e54aad4
Liu233w <wwwlsmcom@outlook.com>
2017-09-18 01:28:42 +0800

    完成图书管理前端 & 前后端交互

---
+++ src/Library.Web.Mvc/wwwroot/view-resources/Pages/BookManage/Index.js
@@ -0,0 +1,57 @@
+﻿(function() {
+    $(function() {
+        var _bookService = abp.services.app.book;
+        var _$modal = $('#BookModal');
+
+        $('#RefreshButton').click(function () {
+            refreshUserList();
+        });
+
+        $('.delete-book').click(function () {
+            var bookId = $(this).attr("data-book-id");
+            var bookTitle = $(this).attr("data-book-title");
+
+            deleteBook(bookId, bookTitle);
+        });
+
+        $('.edit-book').click(function (e) {
+            abp.ui.setBusy(_$modal);
+
+            var bookId = $(this).attr("data-book-id");
+
+            e.preventDefault();
+            $.ajax({
+                url: abp.appPath + 'BookManage/EditBook?bookId=' + bookId,
+                type: 'POST',
+                contentType: 'application/html',
+                success: function (content) {
+                    $('#BookModal div.modal-content').html(content);
+                },
+                error: function (e) { }
+            });
+        });
+
+        _$modal.on('shown.bs.modal', function () {
+            _$modal.find('input:not([type=hidden]):first').focus();
+        });
+
+        function refreshBookList() {
+            location.reload(true); //reload page to see new user!
+        }
+
+        function deleteBook(bookId, bookName) {
+            abp.message.confirm(
+                "Delete book '" + bookName + "'?",
+                function (isConfirmed) {
+                    if (isConfirmed) {
+                        _bookService.delete({
+                            id: bookId
+                        }).done(function () {
+                            refreshBookList();
+                        });
+                    }
+                }
+            );
+        }
+    });
+})();
