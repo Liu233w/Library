824c9e59106c9e70396e3639d9a61f19754efcbb
Liu233w <wwwlsmcom@outlook.com>
2017-09-18 21:54:37 +0800

    数据库添加借阅者编号

---
+++ src/Library.EntityFrameworkCore/Migrations/20170918134413_modify_add_borrower.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Migrations
+{
+    public partial class modify_add_borrower : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "BorrowerUserId",
+                table: "AppBorrowRecords",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RenewTime",
+                table: "AppBorrowRecords",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BorrowerUserId",
+                table: "AppBorrowRecords");
+
+            migrationBuilder.DropColumn(
+                name: "RenewTime",
+                table: "AppBorrowRecords");
+        }
+    }
+}
