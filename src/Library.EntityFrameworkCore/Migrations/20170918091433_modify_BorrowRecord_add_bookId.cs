25e6d2f15a8c30a974bc423f2e74ff21b86a56e4
Liu233w <wwwlsmcom@outlook.com>
2017-09-18 18:40:00 +0800

    [Migration] 给 record 添加 BookId

---
+++ src/Library.EntityFrameworkCore/Migrations/20170918091433_modify_BorrowRecord_add_bookId.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Migrations
+{
+    public partial class modify_BorrowRecord_add_bookId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppBorrowRecords_AppBooks_BookId",
+                table: "AppBorrowRecords");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BookId",
+                table: "AppBorrowRecords",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppBorrowRecords_AppBooks_BookId",
+                table: "AppBorrowRecords",
+                column: "BookId",
+                principalTable: "AppBooks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppBorrowRecords_AppBooks_BookId",
+                table: "AppBorrowRecords");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BookId",
+                table: "AppBorrowRecords",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppBorrowRecords_AppBooks_BookId",
+                table: "AppBorrowRecords",
+                column: "BookId",
+                principalTable: "AppBooks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
