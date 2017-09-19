907dc79c61b36b88a064423093b9545550a7c40f
Liu233w <wwwlsmcom@outlook.com>
2017-09-19 18:04:42 +0800

    添加权限

--- src/Library.Core/Authorization/LibraryAuthorizationProvider.cs
+++ src/Library.Core/Authorization/LibraryAuthorizationProvider.cs
@@ -11,6 +11,8 @@ namespace Library.Authorization
             context.CreatePermission(PermissionNames.Pages_Users, L("Users"));
             context.CreatePermission(PermissionNames.Pages_Roles, L("Roles"));
             context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"), multiTenancySides: MultiTenancySides.Host);
+
+            context.CreatePermission(PermissionNames.Pages_BookManage);
         }
 
         private static ILocalizableString L(string name)
