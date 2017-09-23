2d0b95ef16bec1e5a3e3afd286366f56dd421f30
Liu233w <wwwlsmcom@outlook.com>
2017-09-23 19:58:51 +0800

    bugfix：报错页面无法正常显示
    这是框架的bug

--- src/Library.Web.Mvc/wwwroot/js/admin.js
+++ src/Library.Web.Mvc/wwwroot/js/admin.js
@@ -124,8 +124,11 @@ $.AdminBSB.leftSideBar = {
 
             //Scroll active menu item when page load, if option set = true
             if ($.AdminBSB.options.leftSideBar.scrollActiveItemWhenPageLoad) {
-                var activeItemOffsetTop = $('.menu .list li.active')[0].offsetTop
-                if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+                var el = $('.menu .list li.active')[0];
+                if (el) {
+                    var activeItemOffsetTop = el.offsetTop
+                    if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+                }
             }
         }
     },
