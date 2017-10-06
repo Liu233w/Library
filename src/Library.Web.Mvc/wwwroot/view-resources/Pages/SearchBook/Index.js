7663af1d3168279af3192dcf267df7db31b29163
GeekOrangeLuYao <orangeluyao@gmail.com>
2017-10-06 20:37:55 +0800

    预约界面使用的javascript
    增加的使用javascript去得到BookReserve页面

--- src/Library.Web.Mvc/wwwroot/view-resources/Pages/SearchBook/Index.js
+++ src/Library.Web.Mvc/wwwroot/view-resources/Pages/SearchBook/Index.js
@@ -17,5 +17,18 @@
             })
         });
 
+        $('.show-bookreserve').click(function (e) {
+            var bookId = $(this).attr("data-book-id");
+            e.preventDefault();
+            $.ajax({
+                url: abp.appPath + 'SearchBooks/BookReserve?bookId=' + bookId,
+                type: 'POST',
+                contentType: 'application/html',
+                success: function (content) {
+                    $('#BookReserve div.modal-content').html(content);
+                },
+                error: function(e){ }
+            })
+        })
     });
 })();
