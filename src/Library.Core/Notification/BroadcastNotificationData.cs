abce114987ff69b7a76f5df1d57a2ebffbab3ae1
Liu233w <wwwlsmcom@outlook.com>
2017-10-08 21:38:34 +0800

    feat(Notification): 添加通知定义

---
+++ src/Library.Core/Notification/BroadcastNotificationData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Abp.Notifications;
+
+namespace Library.Notification
+{
+    [Serializable]
+    public class BroadcastNotificationData : NotificationData
+    {
+        public string Content { get; set; }
+
+        public string Publisher { get; set; }
+
+        public BroadcastNotificationData(string content, string publisher)
+        {
+            Content = content;
+            Publisher = publisher;
+        }
+
+        public BroadcastNotificationData()
+        {
+        }
+    }
+}
---
+++ src/Library.Core/Notification/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Notification
+{
+    public static class NotificationType
+    {
+        /// <summary>
+        /// 广播通知
+        /// </summary>
+        public const string BroadcastNotification = "BroadcastNotification";
+    }
+}
