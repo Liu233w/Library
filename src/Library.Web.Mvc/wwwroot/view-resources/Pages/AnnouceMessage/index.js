48891347b08d0912c6aa745143c42be441835c70
GeekOrangeLuYao <orangeluyao@gmail.com>
2017-10-12 20:07:39 +0800

    增加消息页面的Js

---
+++ src/Library.Web.Mvc/wwwroot/view-resources/Pages/AnnouceMessage/index.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    $(function () {
+        //var _notificationService 
+        //var _$modal = $('#NotificationModal');
+
+        //$('.release-message').click(function () {
+        //    var notificationContent = $(this).attr("");
+        //    e.preventDefault();
+        //    $.ajax({
+        //        url: abp.appPath + 'AnnouceMessage/index?content=' + notificationContent,
+        //        type: 'POST',
+        //        contentType: 'application/html',
+        //        success: function (content) {
+        //            $('#BookModal div.modal-content').html(content);
+        //        },
+        //        error: function (e) { }
+        //    });
+        //})
+
+        function refreshNotificationList() {
+            location.reload(this);
+        }
+
+    })
+})  
---
+++ src/Library.Web.Mvc/wwwroot/view-resources/Pages/GetMessage/index.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    $(function () {
+        var _notificationService = abp.services.app.library;
+
+        $('.mark-notification').click(function (e) {
+            //alert("notification-id", notificationid);
+            var notificationid = $(this).attr("data-usernotification-id");
+            markNotification(notificationid);
+        });
+
+        function refreshNotificationList() {
+            location.reload(true); //reload page to see new user!
+        }
+
+        function markNotification(notificationId) {
+            abp.message.confirm(
+                "Mark this Message?",
+                function (isConfirmed) {
+                    if (isConfirmed) {
+                        _notificationService.markNotificationAsRead({
+                            userNotificationId: notificationId
+                        }).done(function () {
+                            refreshNotificationList();
+                        })
+                    }
+                }
+            );
+        }
+    });
+})();
