161f56027b7ee3616ec22690ef40e4e4ef3c16fb
Liu233w <wwwlsmcom@outlook.com>
2017-10-16 11:46:51 +0800

    move to mysql

--- src/Library.EntityFrameworkCore/EntityFrameworkCore/LibraryDbContextConfigurer.cs
+++ src/Library.EntityFrameworkCore/EntityFrameworkCore/LibraryDbContextConfigurer.cs
@@ -7,12 +7,12 @@ namespace Library.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<LibraryDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<LibraryDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            builder.UseMySql(connection);
         }
     }
 }
