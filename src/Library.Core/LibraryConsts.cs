2930b3102590948c8a212161af18951aeaf3fe47
Liu233w <wwwlsmcom@outlook.com>
2017-10-16 20:38:04 +0800

    修改常量：借书时间和最大借书数量

--- src/Library.Core/LibraryConsts.cs
+++ src/Library.Core/LibraryConsts.cs
@@ -13,12 +13,12 @@ namespace Library
         /// <summary>
         /// 用户最多能借几本书
         /// </summary>
-        public const int UserMaxBorrowCount = 5;
+        public const int UserMaxBorrowCount = 2;
 
         /// <summary>
         /// 一本书最多能借几天
         /// </summary>
-        public static readonly TimeSpan UserMaxBorrowDuration = new TimeSpan(14, 0, 0, 0);
+        public static readonly TimeSpan UserMaxBorrowDuration = new TimeSpan(30, 0, 0, 0);
 
         /// <summary>
         /// 续借一次可以借几天
