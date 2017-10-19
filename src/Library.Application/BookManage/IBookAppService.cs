28268a4531a2595bdd631c04d7d8e6a330e398cc
Liu233w <wwwlsmcom@outlook.com>
2017-10-19 12:24:13 +0800

    添加BookAppService文档

--- src/Library.Application/BookManage/IBookAppService.cs
+++ src/Library.Application/BookManage/IBookAppService.cs
@@ -8,6 +8,11 @@ namespace Library.BookManage
     {
         Task DeleteCopy(DeleteCopyInput input);
 
+        /// <summary>
+        /// 获取一本书的所有副本
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         Task<GetCopysOutput> GetCopys(GetCopysInput input);
     }
 }
